f062c48893330af5bc5c9eaae99ebe0c3e93f149
Shankar Ram <Shankar Ram>
2017-04-23 10:06:53 +0200

    Initial commit

---
+++ app/src/main/java/ch/ffhs/fs17/esa/studentcontentprovider/MainActivity.java
@@ -0,0 +1,58 @@
+package ch.ffhs.fs17.esa.studentcontentprovider;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+
+import android.content.ContentValues;
+import android.content.CursorLoader;
+
+import android.database.Cursor;
+
+import android.view.Menu;
+import android.view.View;
+
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClickAddName(View view) {
+        // Add a new student record
+        ContentValues values = new ContentValues();
+        values.put(StudentsProvider.NAME,
+                ((EditText)findViewById(R.id.editTextName)).getText().toString());
+
+        values.put(StudentsProvider.GRADE,
+                ((EditText)findViewById(R.id.editText3)).getText().toString());
+
+        Uri uri = getContentResolver().insert(
+                StudentsProvider.CONTENT_URI, values);
+
+        Toast.makeText(getBaseContext(),
+                uri.toString(), Toast.LENGTH_LONG).show();
+    }
+
+    public void onClickRetrieveStudents(View view) {
+        // Retrieve student records
+        String URL = "content://ch.ffhs.fs17.esa.studentcontentprovider";
+
+        Uri students = Uri.parse(URL);
+        Cursor c = getContentResolver().query(students, null, null, null, StudentsProvider.NAME);
+
+        if (c.moveToFirst()) {
+            do{
+                Toast.makeText(this,
+                        c.getString(c.getColumnIndex(StudentsProvider._ID)) +
+                                ", " +  c.getString(c.getColumnIndex( StudentsProvider.NAME)) +
+                                ", " + c.getString(c.getColumnIndex( StudentsProvider.GRADE)),
+                        Toast.LENGTH_SHORT).show();
+            } while (c.moveToNext());
+        }
+    }
+}
